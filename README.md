d7b9f141979b6e1a020f6709f1cc08dc874146bd
somnathdeveloper <somnathgatkul096@gmail.com>
2024-07-20 22:46:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# samplesrepositoryGitHub
+ 
---
+++ Relational.java
@@ -0,0 +1,26 @@
+
+public class Relational {
+    public static void main(String[] args) {
+
+        /*
+         * ==
+         * !=
+         * >
+         * <
+         * >=
+         * <=
+         */
+
+        // System.out.println(a==b);
+
+        int A = 10;
+        int v = 7;
+        // System.out.println(A!=v);
+        System.out.println(A > v);
+        System.out.println(A / v);
+        System.out.println(A * v);
+        System.out.println(A >= v);
+        System.out.println(A <= v);
+
+    }
+}//
